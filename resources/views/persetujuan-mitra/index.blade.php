a35cc826f082c68b71e1cdb38fec05618a4afec2
Faldian Pratama Wijaya <93422127+faldianpratama@users.noreply.github.com>
2023-06-02 23:55:12 +0700

    nambahin sweet alert

--- resources/views/persetujuan-mitra/index.blade.php
+++ resources/views/persetujuan-mitra/index.blade.php
@@ -6,8 +6,8 @@
             <div class="row">
                 <h3 class="mb-3">Data Pendaftaran Mitra</h3>
                 <br>
-                <table id="table-mitra" class="table table-striped table-bordered display text-center"
-                    width="100%" cellspacing="0">
+                <table id="table-mitra" class="table table-striped table-bordered display text-center" width="100%"
+                    cellspacing="0">
                     <thead style="background-color: #439a97">
                         <tr>
                             <th class="text-center">Nama Venue</th>
@@ -24,24 +24,41 @@
     </div>
 
     <script type="text/javascript">
-        $(document).ready(function(){  
+        $(document).ready(function() {
             var table_mitra = $('#table-mitra').DataTable({
                 processing: true,
                 serverSide: true,
                 responsive: true,
                 ajax: {
-                    url: "{{route('index-sewa')}}",
+                    url: "{{ route('index-sewa') }}",
                     type: 'GET',
                 },
-                columns: [
-                    {data: 'venue_name', name: 'venue_name'},
-                    {data: 'open_hour', name: 'open_hour'},
-                    {data: 'field_qty', name: 'field_qty'},
-                    {data: 'starting_fee', name: 'starting_fee'},
-                    {data: 'status', name: 'status'},
-                    {data: 'action', name: 'action'}
+                columns: [{
+                        data: 'venue_name',
+                        name: 'venue_name'
+                    },
+                    {
+                        data: 'open_hour',
+                        name: 'open_hour'
+                    },
+                    {
+                        data: 'field_qty',
+                        name: 'field_qty'
+                    },
+                    {
+                        data: 'starting_fee',
+                        name: 'starting_fee'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action'
+                    }
                 ],
-                
+
                 order: [
                     [0, 'asc']
                 ],
@@ -49,6 +66,12 @@
         });
     </script>
     @push('css')
+        {{-- Select --}}
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <link rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+
+
         <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
         <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap5.min.css">
         <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.3.1/css/fixedHeader.bootstrap5.min.css">
@@ -56,6 +79,9 @@
     @endpush
 
     @push('scripts')
+        {{-- Select2 --}}
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
         <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
         <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js"></script>
