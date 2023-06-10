8fd0a6ba2c1a94e24eb436174308d050ad2ded6a
NaufalRifki76 <85022839+NaufalRifki76@users.noreply.github.com>
2023-06-10 13:55:24 +0700

    update

--- resources/views/data-pemesanan/index.blade.php
+++ resources/views/data-pemesanan/index.blade.php
@@ -6,37 +6,48 @@
             <div class="row">
                 <h3 class="mb-3">Daftar Data Pemesanan</h3>
                 <br>
-
                 <table id="tabel-data-pemesanan" class="table table-striped table-bordered display text-center" width="100%" cellspacing="0">
                     <thead style="background-color: #439a97">
                         <tr>
                             <th>Nama Pemesan</th>
                             <th>Nama Tempat</th>
                             <th>Tanggal Pemesanan</th>
-                            <th>Jam Bermain</th>
                             <th>Harga Sewa</th>
                             <th>Status</th>
                             <th>Action</th>
                         </tr>
                     </thead>
-                    <tbody class="text-center">
-                        <tr>
-                          <td>Naufal Faris</td>
-                          <td>Vini Vidi Vici Futsal</td>
-                          <td>10/12/2023</td>
-                          <td>09.00 - 10.00<br></td>
-                          <td>150000<br/></td>
-                          <td>Pending, Sukses, Ditolak<br></td>
-                          <td class="d-flex justify-content-center text-center">
-                            <a href="{{ route('data-pemesanan.detail') }}"><button class="btn btn-sm btn-warning me-2 px-3 text-white">Detail</button></a> 
-                            <button class="btn btn-sm btn-danger">Hapus</button>
-                        </td>
-                        </tr>
-                    </tbody>
                 </table>
             </div>
         </div>
     </div>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $table_venue = $('#tabel-data-pemesanan').DataTable({
+                processing: true,
+                serverSide: true,
+                responsive: true,
+                ajax: {
+                    url: "{{route('data-pemesanan.index')}}",
+                    type: 'GET',
+                },
+                columns: [
+                    {data: 'cust_name', name: 'cust_name'},
+                    {data: 'venue_name', name: 'venue_name'},
+                    {data: 'order_date', name: 'order_date'},
+                    {data: 'price_sum', name: 'price_sum'},
+                    {data: 'confirmation', name: 'confirmation'},
+                    {data: 'action', name: 'action'}
+                ],
+                
+                order: [
+                    [0, 'asc']
+                ],
+            });
+        });
+    </script>
+
     @push('css')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap5.min.css">
